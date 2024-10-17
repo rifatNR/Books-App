204460919d44283e259a6b50204c9b2cb1aff2df
rifatNR <Rifatnoor92@gmail.com>
2024-10-17 22:05:29 +0600

    proper fade in animation

--- scripts/book-loader.js
+++ scripts/book-loader.js
@@ -2,7 +2,7 @@ import { delay, createElement } from "./helper.js";
 
 const renderSingleBookCard = (id) => {
     const cardHTML = `
-        <div class="card fade-in">
+        <div id="card_${id}" class="card">
             <div class="card-image-container">
                 <img src="https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg" alt="">
                 <div class="wishlist-icon">
@@ -37,10 +37,15 @@ const renderBooks = async () => {
     }
 
     for (let i = 0; i < 10; i++) {
-        await delay(200);
-        const cardHTML = renderSingleBookCard(i);
+        // await delay(200);
+        const id = i;
+        const cardHTML = renderSingleBookCard(id);
         const newCard = createElement(cardHTML);
         cardContainer.appendChild(newCard);
+
+        setTimeout(() => {
+            document.getElementById(`card_${id}`)?.classList.add("fade-in");
+        }, 50 * i);
     }
 };
 
