202b05bd53eb39356ec7e625d10473756f03656d
rifatNR <Rifatnoor92@gmail.com>
2024-10-18 16:44:08 +0600

    changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 import { initBookLoader } from "/scripts/book-loader.js";
 import { loadHTML } from "/scripts/html-loader.js";
 
-Promise.all([loadHTML("navbar", "/layouts/navbar.html")])
+Promise.all([
+    loadHTML("navbar", "/components/navbar.html"),
+    loadHTML("pagination-container", "/components/pagination.html"),
+])
     .then(() => {
         console.log("All sections are loaded");
 
